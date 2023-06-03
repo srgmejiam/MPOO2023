bcb623df7e06e4b2d4e18b33c2cd0815e298e5fc
Marvin Antonio Mejia Espinoza <SrgMejia@icloud.com>
2023-06-03 12:02:36 -0600

    Creando Arquitectura

---
+++ EL/Cliente.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EL
+{
+    public class Cliente
+    {
+        public int ID { get; set; }
+        public string? Nombre { get; set; }
+        public string? Correo { get; set; }
+        public string? Telefono { get; set; }
+    }
+}
--- MPOO2023/Program.cs
+++ MPOO2023/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hola Alumnos de MPOO");
+﻿static void main()
+{
+
+}
+
+
+
