fbf3488f7e3afabebba20bc41dc2e63fe9aeed17
Marvin Antonio Mejia Espinoza <SrgMejia@icloud.com>
2023-06-17 10:46:07 -0600

    Proyecto a utilizar

--- MPOO2023/Program.cs
+++ MPOO2023/Program.cs
@@ -1,7 +1,25 @@
-﻿static void main()
+﻿using BLL;
+using EL;
+
+static void main()
 {
+    Cliente cliente = new();
+
+    Console.WriteLine("Ingrese su nombre");
+    cliente.Nombre = Console.ReadLine();
+    cliente.IdCliente = 1;
+    BLL_Cliente.InsertCliente(cliente);
+
+
+    foreach (var client in BLL_Cliente.GetClientes())
+    {
+        Console.WriteLine(client.IdCliente + " - " + client.Nombre);
+    }
+    Console.ReadLine();
+
 
 }
+main();
 
 
 
