9e1eafbdc69ae0e6848e7f2ae9e13f993d9e8ac4
Marvin Antonio Mejia Espinoza <SrgMejia@icloud.com>
2023-06-17 10:47:30 -0600

    agregando metodo main

--- MPOO2023/Program.cs
+++ MPOO2023/Program.cs
@@ -1,25 +1,4 @@
-﻿using BLL;
-using EL;
-
-static void main()
+﻿static int main()
 {
-    Cliente cliente = new();
-
-    Console.WriteLine("Ingrese su nombre");
-    cliente.Nombre = Console.ReadLine();
-    cliente.IdCliente = 1;
-    BLL_Cliente.InsertCliente(cliente);
-
-
-    foreach (var client in BLL_Cliente.GetClientes())
-    {
-        Console.WriteLine(client.IdCliente + " - " + client.Nombre);
-    }
-    Console.ReadLine();
-
-
+    return 0;
 }
-main();
-
-
-
