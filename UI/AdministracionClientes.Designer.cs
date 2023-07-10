45b2f1fa5b062bf7aaf9104c961ada8c544df29e
Marvin Antonio Mejia Espinoza <SrgMejia@icloud.com>
2023-07-09 22:04:03 -0600

    Agregando el formulario del video

--- UI/AdministracionClientes.Designer.cs
+++ UI/AdministracionClientes.Designer.cs
@@ -3,12 +3,12 @@
     partial class AdministracionClientes
     {
         /// <summary>
-        ///  Required designer variable.
+        /// Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
         /// <summary>
-        ///  Clean up any resources being used.
+        /// Clean up any resources being used.
         /// </summary>
         /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
@@ -23,139 +23,140 @@
         #region Windows Form Designer generated code
 
         /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
+            this.lblNombreCliente = new System.Windows.Forms.Label();
             this.txtNombreCliente = new System.Windows.Forms.TextBox();
             this.txtNumero = new System.Windows.Forms.TextBox();
-            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             this.txtCorreo = new System.Windows.Forms.TextBox();
-            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnNuevo = new System.Windows.Forms.Button();
             this.btnGuardar = new System.Windows.Forms.Button();
             this.btnAnular = new System.Windows.Forms.Button();
-            this.label4 = new System.Windows.Forms.Label();
             this.gridClientes = new System.Windows.Forms.DataGridView();
-            this.btnReset = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.gridClientes)).BeginInit();
             this.SuspendLayout();
             // 
-            // label1
+            // lblNombreCliente
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(39, 36);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(139, 20);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Nombre del Cliente";
+            this.lblNombreCliente.AutoSize = true;
+            this.lblNombreCliente.Location = new System.Drawing.Point(19, 31);
+            this.lblNombreCliente.Name = "lblNombreCliente";
+            this.lblNombreCliente.Size = new System.Drawing.Size(139, 20);
+            this.lblNombreCliente.TabIndex = 0;
+            this.lblNombreCliente.Text = "Nombre del Cliente";
             // 
             // txtNombreCliente
             // 
-            this.txtNombreCliente.Location = new System.Drawing.Point(38, 69);
+            this.txtNombreCliente.Location = new System.Drawing.Point(19, 54);
             this.txtNombreCliente.Name = "txtNombreCliente";
-            this.txtNombreCliente.Size = new System.Drawing.Size(436, 27);
+            this.txtNombreCliente.Size = new System.Drawing.Size(519, 27);
             this.txtNombreCliente.TabIndex = 1;
             // 
             // txtNumero
             // 
-            this.txtNumero.Location = new System.Drawing.Point(38, 149);
+            this.txtNumero.Location = new System.Drawing.Point(19, 124);
             this.txtNumero.Name = "txtNumero";
-            this.txtNumero.Size = new System.Drawing.Size(436, 27);
+            this.txtNumero.Size = new System.Drawing.Size(519, 27);
             this.txtNumero.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(19, 101);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(144, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Número de teléfono";
+            // 
+            // txtCorreo
+            // 
+            this.txtCorreo.Location = new System.Drawing.Point(19, 201);
+            this.txtCorreo.Name = "txtCorreo";
+            this.txtCorreo.Size = new System.Drawing.Size(519, 27);
+            this.txtCorreo.TabIndex = 5;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(39, 116);
+            this.label2.Location = new System.Drawing.Point(19, 178);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(63, 20);
-            this.label2.TabIndex = 2;
-            this.label2.Text = "Número";
+            this.label2.Size = new System.Drawing.Size(54, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Correo";
             // 
-            // txtCorreo
+            // btnNuevo
             // 
-            this.txtCorreo.Location = new System.Drawing.Point(39, 237);
-            this.txtCorreo.Name = "txtCorreo";
-            this.txtCorreo.Size = new System.Drawing.Size(436, 27);
-            this.txtCorreo.TabIndex = 5;
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(40, 204);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(54, 20);
-            this.label3.TabIndex = 4;
-            this.label3.Text = "Correo";
+            this.btnNuevo.Location = new System.Drawing.Point(19, 263);
+            this.btnNuevo.Name = "btnNuevo";
+            this.btnNuevo.Size = new System.Drawing.Size(94, 29);
+            this.btnNuevo.TabIndex = 6;
+            this.btnNuevo.Text = "Nuevo";
+            this.btnNuevo.UseVisualStyleBackColor = true;
             // 
             // btnGuardar
             // 
-            this.btnGuardar.Location = new System.Drawing.Point(38, 300);
+            this.btnGuardar.Location = new System.Drawing.Point(141, 263);
             this.btnGuardar.Name = "btnGuardar";
             this.btnGuardar.Size = new System.Drawing.Size(94, 29);
-            this.btnGuardar.TabIndex = 6;
+            this.btnGuardar.TabIndex = 7;
             this.btnGuardar.Text = "Guardar";
             this.btnGuardar.UseVisualStyleBackColor = true;
-            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
             // 
             // btnAnular
             // 
-            this.btnAnular.Location = new System.Drawing.Point(154, 300);
+            this.btnAnular.Location = new System.Drawing.Point(268, 263);
             this.btnAnular.Name = "btnAnular";
             this.btnAnular.Size = new System.Drawing.Size(94, 29);
-            this.btnAnular.TabIndex = 7;
+            this.btnAnular.TabIndex = 8;
             this.btnAnular.Text = "Anular";
             this.btnAnular.UseVisualStyleBackColor = true;
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(38, 356);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(134, 20);
-            this.label4.TabIndex = 8;
-            this.label4.Text = "Listado de Clientes";
             // 
             // gridClientes
             // 
             this.gridClientes.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.gridClientes.Location = new System.Drawing.Point(38, 379);
+            this.gridClientes.Location = new System.Drawing.Point(21, 342);
             this.gridClientes.Name = "gridClientes";
             this.gridClientes.RowHeadersWidth = 51;
             this.gridClientes.RowTemplate.Height = 29;
-            this.gridClientes.Size = new System.Drawing.Size(436, 188);
+            this.gridClientes.Size = new System.Drawing.Size(517, 303);
             this.gridClientes.TabIndex = 9;
             // 
-            // btnReset
+            // label3
             // 
-            this.btnReset.Location = new System.Drawing.Point(280, 300);
-            this.btnReset.Name = "btnReset";
-            this.btnReset.Size = new System.Drawing.Size(94, 29);
-            this.btnReset.TabIndex = 10;
-            this.btnReset.Text = "Reset";
-            this.btnReset.UseVisualStyleBackColor = true;
-            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(21, 319);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(134, 20);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Listado de Clientes";
             // 
             // AdministracionClientes
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(513, 611);
-            this.Controls.Add(this.btnReset);
+            this.ClientSize = new System.Drawing.Size(565, 657);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.gridClientes);
-            this.Controls.Add(this.label4);
             this.Controls.Add(this.btnAnular);
             this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.btnNuevo);
             this.Controls.Add(this.txtCorreo);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.txtNumero);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.txtNombreCliente);
+            this.Controls.Add(this.txtNumero);
             this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtNombreCliente);
+            this.Controls.Add(this.lblNombreCliente);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MaximizeBox = false;
             this.Name = "AdministracionClientes";
-            this.Text = "Clientes";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "AdministracionClientes";
             this.Load += new System.EventHandler(this.AdministracionClientes_Load);
             ((System.ComponentModel.ISupportInitialize)(this.gridClientes)).EndInit();
             this.ResumeLayout(false);
@@ -165,16 +166,16 @@
 
         #endregion
 
-        private Label label1;
+        private Label lblNombreCliente;
         private TextBox txtNombreCliente;
         private TextBox txtNumero;
-        private Label label2;
+        private Label label1;
         private TextBox txtCorreo;
-        private Label label3;
+        private Label label2;
+        private Button btnNuevo;
         private Button btnGuardar;
         private Button btnAnular;
-        private Label label4;
         private DataGridView gridClientes;
-        private Button btnReset;
+        private Label label3;
     }
 }
--- UI/AdministracionClientes.cs
+++ UI/AdministracionClientes.cs
@@ -1,5 +1,13 @@
-using EL;
-using BLL;
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
 namespace UI
 {
     public partial class AdministracionClientes : Form
@@ -8,43 +16,10 @@ namespace UI
         {
             InitializeComponent();
         }
-      
+
         private void AdministracionClientes_Load(object sender, EventArgs e)
         {
-            cargarGrid();
-        }
 
-        private void cargarGrid()
-        {
-            gridClientes.DataSource = BLL_Clientes.Lista();
-        }
-        private void Guardar()
-        {
-            Clientes Cliente = new();
-            Cliente.NombreCliente = txtNombreCliente.Text;
-            Cliente.Numero = txtNumero.Text;
-            Cliente.Correo= txtCorreo.Text;
-            BLL_Clientes.Insert(Cliente);
-
-        }
-        private void LimpiarCampos()
-        {
-            txtNombreCliente.Text = string.Empty;
-            txtCorreo.Text = string.Empty;  
-            txtNumero.Text = string.Empty;  
-        }
-
-        private void btnGuardar_Click(object sender, EventArgs e)
-        {
-            Guardar();
-            cargarGrid();
-            LimpiarCampos();
-        }
-
-        private void btnReset_Click(object sender, EventArgs e)
-        {
-            LimpiarCampos();
-            cargarGrid();
         }
     }
 }
